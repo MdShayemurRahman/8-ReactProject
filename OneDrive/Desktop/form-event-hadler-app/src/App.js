b31ba6471949e255d2657e67d05f88f129cbf2e3
Md Shayemur Rahman <sayemurrahman99@gmail.com>
2022-04-25 22:17:51 +0300

    event-hanlder-basic-practices

---
+++ OneDrive/Desktop/form-event-hadler-app/src/App.js
@@ -0,0 +1,12 @@
+import NameApp from './components/NameApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NameApp />
+    </div>
+  );
+}
+
+export default App;
